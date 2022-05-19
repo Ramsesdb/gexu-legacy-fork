cuong-tran/tachiyomiJ2K

change Track.last_chapter_read to Float
each TrackService can convert it to Int if decimal chapters are not supported

Co-Authored-By: Gauthier <2139133+gotson@users.noreply.github.com>

app/src/main/java/eu/kanade/tachiyomi/ui/manga/track/TrackingBottomSheet.kt
@@ -469,7 +469,7 @@ class TrackingBottomSheet(private val controller: MangaDetailsController) :
 
         val np = binding.chaptersPicker
         // Set initial value
-        np.value = item.track.last_chapter_read
+        np.value = item.track.last_chapter_read.toInt()
         if (item.track.total_chapters > 0) {
             np.wrapSelectorWheel = true
             np.maxValue = item.track.total_chapters

Ramsesdb/gexu-legacy-fork

(cherry picked from commit 2fec20ed4ad6869d43aa7f17b68291cb3ad287f5)

app/src/main/java/eu/kanade/tachiyomi/ui/manga/track/TrackingBottomSheet.kt
@@ -453,7 +453,7 @@ class TrackingBottomSheet(private val controller: MangaDetailsController) :
 
         val np = binding.chaptersPicker
         // Set initial value
-        np.progress = item.track.last_chapter_read
+        np.progress = item.track.last_chapter_read.toInt()
         if (item.track.total_chapters > 0) {
             np.maxValue = item.track.total_chapters
         }